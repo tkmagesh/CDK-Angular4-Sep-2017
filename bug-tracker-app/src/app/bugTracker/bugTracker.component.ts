6f0421fa5e98f0961be961538b8b053f894eac2f
Magesh <tkmagesh77@gmail.com>
2017-09-14 15:47:59 +0530

    use cases 3, 4  & 5

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { IBug } from '../models/IBug';
+
+
 
 @Component({
 	selector : 'bug-tracker',
@@ -6,9 +9,29 @@ import { Component } from '@angular/core';
 	styleUrls : ['bugTracker.component.css']
 })
 export class BugTrackerComponent{
-	bugs : string[] = [];
+	bugs : IBug[] = [];
 
 	addNew(bugName : string){
-		this.bugs.push(bugName);
+		let newBug : IBug = {
+			name : bugName,
+			isClosed : false
+		}
+		this.bugs.push(newBug);
+	}
+	toggle(bug : IBug){
+		bug.isClosed = !bug.isClosed;
+	}
+	removeClosed(){
+		for(let index = this.bugs.length-1; index >= 0; index--){
+			if (this.bugs[index].isClosed)
+				this.bugs.splice(index, 1);
+		}
+	}
+	getClosedCount(){
+		var closedCount = 0;
+		for(let index = 0, count = this.bugs.length; index < count; index++)
+			if (this.bugs[index].isClosed)
+				++closedCount;
+		return closedCount;
 	}
 }
